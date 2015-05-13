7ba868655d538ebdc6cbbcc837e2c167a8712167
snow <Yanghaodong125@163.com>
2015-05-13 21:47:15 +0800

    to do it

---
+++ src/com/yhd/tabview/TabView.java
@@ -0,0 +1,24 @@
+package com.yhd.tabview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class TabView extends LinearLayout{
+
+	private int tabCount;
+	
+	public TabView(Context context) {
+		super(context);
+	}
+
+	public TabView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public TabView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+
+}
