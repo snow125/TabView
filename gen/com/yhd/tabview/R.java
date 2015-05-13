76647ae9006591999853395bd75018ef3ec4f2ab
snow <Yanghaodong125@163.com>
2015-05-14 00:20:11 +0800

    prepare reflect to get R drawable selector

--- gen/com/yhd/tabview/R.java
+++ gen/com/yhd/tabview/R.java
@@ -8,6 +8,10 @@
 package com.yhd.tabview;
 
 public final class R {
+    public static final class array {
+        public static final int pic_normal=0x7f040000;
+        public static final int pic_selected=0x7f040001;
+    }
     public static final class attr {
         /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
@@ -34,25 +38,28 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int normal=0x7f020001;
+        public static final int selected=0x7f020002;
+        public static final int selector=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -76,11 +83,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class styleable {
         /** Attributes that can be used with a tab_view.
--- src/com/yhd/tabview/TabView.java
+++ src/com/yhd/tabview/TabView.java
@@ -1,24 +1,86 @@
 package com.yhd.tabview;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class TabView extends LinearLayout{
 
+	private Context context;
 	private int tabCount;
+	private RelativeLayout[] midViews;
+	private ImageView[] childViews;
+	private String[] picNormal;
+	private String[] picSelected;
+	private int itemWidth;
 	
 	public TabView(Context context) {
-		super(context);
-	}
-
-	public TabView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
+		this(context, null, 0);
 	}
 
 	public TabView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		this(context, attrs, 0);
 	}
 	
+	public TabView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		this.context = context;
+		TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.tab_view, defStyle, 0);
+		int count = a.getIndexCount();
+		for (int i = 0; i < count; i++) {
+			int attr = a.getIndex(i);
+			switch (attr) {
+			case R.styleable.tab_view_tab_count:
+				tabCount = a.getInteger(attr, 0);
+				break;
+			case R.styleable.tab_view_tab_pic_normal:
+				picNormal = getResources().getStringArray(a.getResourceId(attr, 0));	
+				break;
+			case R.styleable.tab_view_tab_pic_selected:
+				picSelected = getResources().getStringArray(a.getResourceId(attr, 0));
+				break;
+			}
+		}
+		setParentView();
+		addMidView();
+		addChildView();
+		a.recycle();
+	}
+	
+	private void setParentView() {
+		this.setOrientation(HORIZONTAL);
+		midViews = new RelativeLayout[tabCount];
+		childViews = new ImageView[tabCount];
+		itemWidth = ScreenTool.getScreenWidth(context)/tabCount;
+	}
+	
+	private void addMidView() {
+		for (int i = 0; i < tabCount; i++) {
+			midViews[i] = new RelativeLayout(context);
+			RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(itemWidth, android.widget.RelativeLayout.LayoutParams.MATCH_PARENT);
+			midViews[i].setLayoutParams(lp);
+			addView(midViews[i]);
+		}
+	}
+	
+	@SuppressLint("NewApi")
+	private void addChildView() {
+		for (int i = 0; i < tabCount; i++) {
+			childViews[i] = new ImageView(context);
+			Log.e("123", ""+picNormal[i]);
+			childViews[i].setBackgroundResource(getResId(picNormal[i]));
+			midViews[i].addView(childViews[i]);
+		}
+	}
+	
+	private int getResId(String res){
+		return 0x7f020003;
+	}
 
 }
